9076e2165c335a853e51ceae0c5d42a7b730546e
Laryssa Adrielly  dos Santos Soares <80859127+adrielaryssa@users.noreply.github.com>
2021-03-27 17:52:14 -0300

    Add files via upload

---
+++ Botao.java
@@ -0,0 +1,19 @@
+
+package botao;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+
+public class Botao extends JFrame{
+ private JButton ok;
+ 
+ public Botao(){
+  super("Criando botões");
+  setLayout( new FlowLayout() );
+
+  ok = new JButton("Start");
+  add(ok);
+ }
+
+}
---
+++ GUI.java
@@ -0,0 +1,16 @@
+
+package botao;
+
+import javax.swing.JFrame;
+
+public class GUI {
+ public static void main(String[] args) {
+  Botao botao1 = new Botao();
+  
+  botao1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+  botao1.setSize(350,350);
+  botao1.setVisible(true);
+ }
+}
+
+
